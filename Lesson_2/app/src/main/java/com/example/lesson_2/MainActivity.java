a5a4baa0c6c922ab49ec98467841bb588f68be00
Telefonica95 <diminti@hotmail.com>
2020-03-02 01:09:09 +0100

    First 2 Lessons

---
+++ Lesson_2/app/src/main/java/com/example/lesson_2/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.lesson_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+Button myMainButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//
+//        myMainButton = findViewById(R.id.button);
+//        myMainButton.setOnClickListener(new View.OnClickListener()){
+//            @Override
+//                    public void opnClick(View v){
+//                for (int i = 0; i <10 ; i++) {
+//                    if ()
+//                }
+//            }
+//        }
+
+    }
+
+}
